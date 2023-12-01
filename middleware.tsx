39f907978ed0dc895a53bdbf9e006e28690f07a0
Mathias Bragagia <mathias.bragagia@epitech.eu>
2023-12-01 09:33:38 +0100

    Bugibog

--- middleware.tsx
+++ middleware.tsx
@@ -18,14 +18,24 @@ export async function middleware(req: NextRequest) {
 
     // if user is signed in and the current path is / redirect the user to /account
     if (session && req.nextUrl.pathname === "/") {
-      console.log("redirected1");
-      return NextResponse.redirect(URLS.workspaceIndex);
+      console.log(
+        "redirected1: ",
+        new URL(URLS.workspaceIndex, process.env.NEXT_PUBLIC_URL!).toString()
+      );
+      return NextResponse.redirect(
+        new URL(URLS.workspaceIndex, process.env.NEXT_PUBLIC_URL!)
+      );
     }
 
     // if user is not signed in and the current path is not / redirect the user to /
     if (!session && req.nextUrl.pathname !== "/") {
-      console.log("redirected2");
-      return NextResponse.redirect(URLS.login);
+      console.log(
+        "redirected2: ",
+        new URL(URLS.login, process.env.NEXT_PUBLIC_URL!).toString()
+      );
+      return NextResponse.redirect(
+        new URL(URLS.login, process.env.NEXT_PUBLIC_URL!)
+      );
     }
 
     console.log("hasSession: ", session ? true : false);
