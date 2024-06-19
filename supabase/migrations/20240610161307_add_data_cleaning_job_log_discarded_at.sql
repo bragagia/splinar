8ef935fdf3b771d140f32825daf3a9ec97e9c6fd
Mathias Bragagia <mathias.bragagia@epitech.eu>
2024-06-19 19:50:40 +0200

    Fix past migration error

--- supabase/migrations/20240610161307_add_data_cleaning_job_log_discarded_at.sql
+++ supabase/migrations/20240610161307_add_data_cleaning_job_log_discarded_at.sql
@@ -1,4 +1,4 @@
 ALTER TABLE data_cleaning_job_logs ADD COLUMN discarded_at TIMESTAMP WITH TIME ZONE;
 
 DROP INDEX IF EXISTS data_cleaning_job_logs_workspace_id_data_cleaning_job_id_item_id_index;
-CREATE UNIQUE INDEX data_cleaning_job_logs_workspace_id_data_cleaning_job_id_item_id_index ON data_cleaning_job_logs (workspace_id, data_cleaning_job_id, item_id, accepted_at, discarded_at) WHERE accepted_at IS NULL AND discarded_at IS NULL;
+CREATE UNIQUE INDEX data_cleaning_job_logs_workspace_id_data_cleaning_job_id_item_id_index ON data_cleaning_job_logs (workspace_id, data_cleaning_job_id, item_id) WHERE accepted_at IS NULL AND discarded_at IS NULL;
