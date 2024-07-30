7941c530026ec22c97c7d845fba8110c5777d29d
Mathias Bragagia <mathias.bragagia@epitech.eu>
2024-07-30 17:20:18 +0200

    Add deals to hubspot oauth

--- lib/urls.tsx
+++ lib/urls.tsx
@@ -40,7 +40,7 @@ export const URLS = {
     termsOfService: "https://splinar.com/terms",
     privacyPolicy: "https://splinar.com/privacy-policy",
     hubspotOAuth:
-      "https://app-eu1.hubspot.com/oauth/authorize?client_id=1e3368cb-a326-4217-943d-0f9a14a91f07&redirect_uri=https://app.splinar.com/hubspot/callback&scope=crm.objects.contacts.read%20crm.objects.contacts.write%20crm.objects.companies.write%20crm.schemas.contacts.read%20crm.objects.companies.read%20crm.schemas.companies.read", // TODO: Need to create local dev hubspot app
+      "https://app-eu1.hubspot.com/oauth/authorize?client_id=1e3368cb-a326-4217-943d-0f9a14a91f07&redirect_uri=https://app.splinar.com/hubspot/callback&scope=crm.objects.contacts.write%20crm.objects.companies.write%20crm.objects.companies.read%20crm.objects.deals.read%20crm.schemas.contacts.read%20crm.objects.deals.write%20crm.objects.contacts.read%20crm.schemas.companies.read", // TODO: Need to create local dev hubspot app
     hubspotContact: (workspaceHubId: string, distantId: string) =>
       "https://app.hubspot.com/contacts/" +
       workspaceHubId +
